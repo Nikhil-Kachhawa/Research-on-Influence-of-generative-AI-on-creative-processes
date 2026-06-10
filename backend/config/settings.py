5fcb72e67ce1d5c974deac7fed4450202317a3b7
Nikhil-Kachhawa <nikhilkachhawa31@gmail.com>
2026-06-10 20:24:04 +0530

    Configure Render deployment

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -23,9 +23,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-!*ekll&%g@c_jicl_c+%$a-4fw2=(t-9qdi6kp538$-ekoxo=0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+import os
+DEBUG = os.environ.get("DEBUG", "False") == "True" # prod
 
-ALLOWED_HOSTS = []
+# ALLOWED_HOSTS = [] #local
+
+ALLOWED_HOSTS = ["*"] # prod
 
 
 # Application definition
@@ -43,8 +46,10 @@ INSTALLED_APPS = [
     "chatbot",
 ]
 
+#For prod added "whitenoise.middleware.WhiteNoiseMiddleware", right after SecurityMiddleware
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -53,6 +58,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 
     "corsheaders.middleware.CorsMiddleware",
+    
 ]
 
 ROOT_URLCONF = 'config.urls'
@@ -121,5 +127,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATIC_ROOT = BASE_DIR / "staticfiles"
 CORS_ALLOW_ALL_ORIGINS = True
