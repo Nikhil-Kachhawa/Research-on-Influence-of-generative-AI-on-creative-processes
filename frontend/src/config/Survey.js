d88433cee294a018a3d24418424d59c2459d252f
Nikhil-Kachhawa <nikhilkachhawa31@gmail.com>
2026-07-09 14:46:21 +0530

    feat: update survey URLs for production and comment out development links

--- frontend/src/config/Survey.js
+++ frontend/src/config/Survey.js
@@ -1,10 +1,10 @@
 export const SURVEY = {
   //prod
-  // SURVEY_1: "https://sosci.rlp.net/Research_Lab_survey_01_1/?q=base", 
-  // SURVEY_2: "https://sosci.rlp.net/research_lab_survey_02_1/?q=base",
+  SURVEY_1: "https://sosci.rlp.net/Research_Lab_survey_01_1/?q=base", 
+  SURVEY_2: "https://sosci.rlp.net/research_lab_survey_02_1/?q=base",
 
   //dev
-  SURVEY_1: "https://sosci.rlp.net/research_lab_survey_1/?q=base",
-  SURVEY_2: "https://sosci.rlp.net/research_lab_survey_2/?q=base",
+  // SURVEY_1: "https://sosci.rlp.net/research_lab_survey_1/?q=base",
+  // SURVEY_2: "https://sosci.rlp.net/research_lab_survey_2/?q=base",
 
 };
